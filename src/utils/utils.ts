ab8d980200ac6326a730cefc07fd847137359a1a
Ethan Wallace <ethan.wallace.91@gmail.com>
2022-04-21 07:53:25 -0400

    Fix overwriting of inherited attributes (#14)

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,14 +2,14 @@ export function format(label: string): string {
   return (label ? ` ${label}` : 'Fallback Button Label');
 }
 
-export const inheritAttributes = (el: HTMLElement, attributes: string[] = []) => {
+export const inheritAttributes = (el: HTMLElement, shadowElement: HTMLElement, attributes: string[] = []) => {
   const attributeObject = {};
 
   attributes.forEach(attr => {
-    if (el.hasAttribute(attr)) {
-      const value = el.getAttribute(attr);
+    if (el.hasAttribute(attr) || (shadowElement && shadowElement.hasAttribute(attr))) {
+      const value = el.getAttribute(attr) || shadowElement.getAttribute(attr);
       if (value !== null) {
-        attributeObject[attr] = el.getAttribute(attr);
+        attributeObject[attr] = el.getAttribute(attr) || shadowElement.getAttribute(attr);
       }
       el.removeAttribute(attr);
     }
