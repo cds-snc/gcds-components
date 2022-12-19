0fb23f48f089d0c0a854db59fde51a3f641e6b52
Ethan Wallace <ethan.wallace.91@gmail.com>
2022-12-19 13:56:39 -0500

    Date modified component (#76)
    * Date modified component
    
    * Restore index.html page
    
    * PR review
    
    * Fix French string

--- packages/react/src/components/stencil-generated/index.ts
+++ packages/react/src/components/stencil-generated/index.ts
@@ -11,6 +11,7 @@ defineCustomElements();
 export const GcdsAlert = /*@__PURE__*/createReactComponent<JSX.GcdsAlert, HTMLGcdsAlertElement>('gcds-alert');
 export const GcdsButton = /*@__PURE__*/createReactComponent<JSX.GcdsButton, HTMLGcdsButtonElement>('gcds-button');
 export const GcdsCheckbox = /*@__PURE__*/createReactComponent<JSX.GcdsCheckbox, HTMLGcdsCheckboxElement>('gcds-checkbox');
+export const GcdsDateModified = /*@__PURE__*/createReactComponent<JSX.GcdsDateModified, HTMLGcdsDateModifiedElement>('gcds-date-modified');
 export const GcdsDetails = /*@__PURE__*/createReactComponent<JSX.GcdsDetails, HTMLGcdsDetailsElement>('gcds-details');
 export const GcdsErrorMessage = /*@__PURE__*/createReactComponent<JSX.GcdsErrorMessage, HTMLGcdsErrorMessageElement>('gcds-error-message');
 export const GcdsFieldset = /*@__PURE__*/createReactComponent<JSX.GcdsFieldset, HTMLGcdsFieldsetElement>('gcds-fieldset');
