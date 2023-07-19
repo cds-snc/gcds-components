ea5efe07ae81f4b0c0d9f1d05c26f74a14dcac06
ethanWallace <ethanWallace@users.noreply.github.com>
2023-07-19 13:02:39 +0000

    deploy: edcb6550f42f14275043cea96b6d4ddd9b7dd35c

---
+++ sb-addons/storybook-10/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var c=__STORYBOOKADDONS__,{addons:a,types:g,mockChannel:_}=__STORYBOOKADDONS__;var T=__STORYBOOKTHEMING__,{CacheProvider:u,ClassNames:b,Global:x,ThemeProvider:B,background:S,color:v,convert:R,create:t,createCache:G,createGlobal:N,createReset:y,css:k,darken:D,ensure:I,ignoreSsrWarning:K,isPropValid:Y,jsx:P,keyframes:A,lighten:E,styled:H,themes:M,typography:j,useTheme:w,withTheme:U}=__STORYBOOKTHEMING__;var n=t({base:"light",brandTitle:"Government of Canada | Gouvernement du Canada",brandUrl:"",brandImage:"https://digital.canada.ca/img/cds/goc--header-logo.svg",brandTarget:"_self",fontBase:'"Noto Sans", sans-serif',fontCode:"monospace",appContentBg:"#fff",appBorderColor:"#7d828b",appBorderRadius:0,barTextColor:"#000",textColor:"#000",textInverseColor:"green",inputBorderRadius:"0.1875rem"});a.setConfig({theme:n});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
