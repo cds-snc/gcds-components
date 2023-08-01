28b1bfdc9495efe694c018a668e2752a743e9404
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-08-01 13:58:33 -0400

    Search unit test + update test for button and header

--- packages/web/src/components/gcds-search/test/gcds-search.e2e.ts
+++ packages/web/src/components/gcds-search/test/gcds-search.e2e.ts
@@ -1,4 +1,5 @@
 import { newE2EPage } from '@stencil/core/testing';
+const { AxePuppeteer } = require('@axe-core/puppeteer');
 
 describe('gcds-search', () => {
   it('renders', async () => {
@@ -7,5 +8,43 @@ describe('gcds-search', () => {
 
     const element = await page.find('gcds-search');
     expect(element).toHaveClass('hydrated');
+  });
+});
+
+
+/**
+   * Accessibility tests
+   * Axe-core rules: https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md#wcag-21-level-a--aa-rules
+   */
+
+describe('gcds-search a11y tests', () => {
+  /**
+   * Colour contrast test
+   */
+  it('colour contrast', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <gcds-search />
+    `);
+    
+    const colorContrastTest = new AxePuppeteer(page).withRules('color-contrast').analyze();
+    let results = await colorContrastTest;
+
+    expect(results.violations.length).toBe(0);
+  });
+
+  /**
+   * Buttons have discernible text
+   */
+  it('Button name', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <gcds-search />
+    `);
+    
+    const colorContrastTest = new AxePuppeteer(page).withRules('button-name').analyze();
+    let results = await colorContrastTest;
+
+    expect(results.violations.length).toBe(0);
   });
 });
--- packages/web/src/components/gcds-search/test/gcds-search.spec.tsx
+++ packages/web/src/components/gcds-search/test/gcds-search.spec.tsx
@@ -2,16 +2,75 @@ import { newSpecPage } from '@stencil/core/testing';
 import { GcdsSearch } from '../gcds-search';
 
 describe('gcds-search', () => {
-  it('renders', async () => {
+  it('renders w/ defaults', async () => {
     const page = await newSpecPage({
       components: [GcdsSearch],
       html: `<gcds-search></gcds-search>`,
     });
     expect(page.root).toEqualHtml(`
       <gcds-search>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
+        <section class="gcds-search">
+          <h2 class="gcds-search__header">
+            Search
+          </h2>
+          <form action="https://www.canada.ca/en/sr/srb.html" class="gcds-search__form" method="get" role="search">
+            <gcds-label hide-label="" label="Search Canada.ca" label-for="search"></gcds-label>
+            <input class="gcds-search__input" id="search" list="search-list" maxlength="170" name="q" placeholder="Search Canada.ca" size="34" type="search">
+            <gcds-button class="gcds-search__button" exportparts="button" type="submit">
+              <gcds-icon fixed-width="" label="Search" name="search"></gcds-icon>
+            </gcds-button>
+          </form>
+        </section>
+      </gcds-search>
+    `);
+  });
+  it('renders french', async () => {
+    const page = await newSpecPage({
+      components: [GcdsSearch],
+      html: `<gcds-search lang="fr"></gcds-search>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <gcds-search lang="fr">
+      <section class="gcds-search">
+          <h2 class="gcds-search__header">
+            Recherche
+          </h2>
+          <form action="https://www.canada.ca/fr/sr/srb.html" class="gcds-search__form" method="get" role="search">
+            <gcds-label hide-label="" label="Recherche dans Canada.ca" label-for="search"></gcds-label>
+            <input class="gcds-search__input" id="search" list="search-list" maxlength="170" name="q" placeholder="Recherche dans Canada.ca" size="34" type="search">
+            <gcds-button class="gcds-search__button" exportparts="button" type="submit">
+              <gcds-icon fixed-width="" label="Recherche" name="search"></gcds-icon>
+            </gcds-button>
+          </form>
+        </section>
+      </gcds-search>
+    `);
+  });
+  it('renders w/ custom properties', async () => {
+    const page = await newSpecPage({
+      components: [GcdsSearch],
+      html: `<gcds-search
+        search-id="searchForm"
+        name="s"
+        method="post"
+        action="submit.html"
+        placeholder="Text.ca"
+      ></gcds-search>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <gcds-search action="submit.html" method="post" name="s" placeholder="Text.ca" search-id="searchForm">
+        <section class="gcds-search">
+          <h2 class="gcds-search__header">
+            Search
+          </h2>
+          <form action="submit.html" class="gcds-search__form" method="post" role="search">
+          <gcds-label hide-label="" label="Search Text.ca" label-for="searchForm"></gcds-label>
+            <input class="gcds-search__input" id="searchForm" list="search-list" maxlength="170" name="s" placeholder="Search Text.ca" size="34" type="search">
+            <gcds-button class="gcds-search__button" exportparts="button" type="submit">
+              <gcds-icon fixed-width="" label="Search" name="search"></gcds-icon>
+            </gcds-button>
+          </form>
+        </section>
       </gcds-search>
     `);
   });
