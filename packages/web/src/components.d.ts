1ab4b1e557965b8fd86ce9deef9376b7a5b3b0be
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-08-02 13:54:47 -0400

    Update tokens package

--- packages/web/src/components.d.ts
+++ packages/web/src/components.d.ts
@@ -992,6 +992,10 @@ export interface GcdsRadioCustomEvent<T> extends CustomEvent<T> {
     detail: T;
     target: HTMLGcdsRadioElement;
 }
+export interface GcdsSearchCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLGcdsSearchElement;
+}
 export interface GcdsSelectCustomEvent<T> extends CustomEvent<T> {
     detail: T;
     target: HTMLGcdsSelectElement;
@@ -1157,6 +1161,12 @@ declare global {
         prototype: HTMLGcdsRadioElement;
         new (): HTMLGcdsRadioElement;
     };
+    interface HTMLGcdsSearchElement extends Components.GcdsSearch, HTMLStencilElement {
+    }
+    var HTMLGcdsSearchElement: {
+        prototype: HTMLGcdsSearchElement;
+        new (): HTMLGcdsSearchElement;
+    };
     interface HTMLGcdsSelectElement extends Components.GcdsSelect, HTMLStencilElement {
     }
     var HTMLGcdsSelectElement: {
@@ -1226,6 +1236,7 @@ declare global {
         "gcds-pagination": HTMLGcdsPaginationElement;
         "gcds-phase-banner": HTMLGcdsPhaseBannerElement;
         "gcds-radio": HTMLGcdsRadioElement;
+        "gcds-search": HTMLGcdsSearchElement;
         "gcds-select": HTMLGcdsSelectElement;
         "gcds-side-nav": HTMLGcdsSideNavElement;
         "gcds-signature": HTMLGcdsSignatureElement;
@@ -2341,6 +2352,7 @@ declare namespace LocalJSX {
         "gcds-pagination": GcdsPagination;
         "gcds-phase-banner": GcdsPhaseBanner;
         "gcds-radio": GcdsRadio;
+        "gcds-search": GcdsSearch;
         "gcds-select": GcdsSelect;
         "gcds-side-nav": GcdsSideNav;
         "gcds-signature": GcdsSignature;
@@ -2380,6 +2392,7 @@ declare module "@stencil/core" {
             "gcds-pagination": LocalJSX.GcdsPagination & JSXBase.HTMLAttributes<HTMLGcdsPaginationElement>;
             "gcds-phase-banner": LocalJSX.GcdsPhaseBanner & JSXBase.HTMLAttributes<HTMLGcdsPhaseBannerElement>;
             "gcds-radio": LocalJSX.GcdsRadio & JSXBase.HTMLAttributes<HTMLGcdsRadioElement>;
+            "gcds-search": LocalJSX.GcdsSearch & JSXBase.HTMLAttributes<HTMLGcdsSearchElement>;
             "gcds-select": LocalJSX.GcdsSelect & JSXBase.HTMLAttributes<HTMLGcdsSelectElement>;
             "gcds-side-nav": LocalJSX.GcdsSideNav & JSXBase.HTMLAttributes<HTMLGcdsSideNavElement>;
             "gcds-signature": LocalJSX.GcdsSignature & JSXBase.HTMLAttributes<HTMLGcdsSignatureElement>;
