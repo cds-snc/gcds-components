d4d6f9fbb79437a06f2316b5e1ffea5c55110f38
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-09-12 17:10:01 -0400

    Gcds-header: Remove label from nav landmark to avoid confusion (#291)
    Remove label from nav landmark to avoid confusion

--- packages/web/src/components/gcds-header/gcds-header.tsx
+++ packages/web/src/components/gcds-header/gcds-header.tsx
@@ -64,7 +64,7 @@ export class GcdsHeader {
       return <slot name="skip-to-nav"></slot>;
     } else if (this.skipToHref) {
       return (
-        <nav aria-label={i18n[this.lang].skip} class="gcds-header__skip-to-nav">
+        <nav class="gcds-header__skip-to-nav">
           <gcds-button
             type="link"
             button-role="skip-to-content"
