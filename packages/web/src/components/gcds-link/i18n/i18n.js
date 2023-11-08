e73ae7d08bf23a0de9f3ec835001fece5f39f6a4
Daine Trinidad <daine.trinidad@cds-snc.ca>
2023-11-07 20:05:57 -0800

    chore: updated text and i18n

--- packages/web/src/components/gcds-link/i18n/i18n.js
+++ packages/web/src/components/gcds-link/i18n/i18n.js
@@ -1,15 +1,15 @@
 const I18N = {
   en: {
-    external: 'Opens in a new tab.',
-    phone: 'Opens a phone app.',
-    download: 'Downloads a file.',
-    email: 'Opens an email client.',
+    external: 'Opens destination in a new tab.',
+    phone: 'Attempts to open a phone app.',
+    download: 'Attempts to download a file to the device.',
+    email: 'Opens new message in email program.',
   },
   fr: {
-    external: "S'ouvre dans un nouvel onglet.",
-    phone: 'Phone number [fr & copy edit needed]',
-    download: 'Download [fr & copy edit needed]',
-    email: 'Email [fr & copy edit needed]',
+    external: "Ouvre l'emplacement dans un nouvel onglet.",
+    phone: "Tente d'ouvrir une application de téléphonie",
+    download: "Tente de télécharger un fichier sur l'appareil.",
+    email: 'Ouvre un nouveau message dans le logiciel de messagerie.',
   },
 };
 
