bc33868c6038bed52e71a59037809b75fc203d29
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-12-04 12:37:24 -0500

    React components now require camelCase attributes

--- packages/ssr/scripts/utils.ts
+++ packages/ssr/scripts/utils.ts
@@ -64,7 +64,7 @@ export const toModuleFile = (
 
 import React, { useImperativeHandle, useRef } from 'react';
 import { ${defineCustomElementFunction} } from '${importPath}';
-import { omitEventCallbacks, toNativeProps, useEventListeners } from './lib/utils.js';
+import { omitEventCallbacks, useEventListeners } from './lib/utils.js';
 
 if (!customElements.get('${elementName}')) {
   ${defineCustomElementFunction}();
@@ -72,12 +72,17 @@ if (!customElements.get('${elementName}')) {
 
 const customEvents = ${customEvents.length > 0 ? `['${customEvents.join(`', '`)}']` : '[]'};
 const ${toPascalCase(elementName)} = React.forwardRef(({ children = [], ...props }, ref) => {
-  const nativeProps = toNativeProps(omitEventCallbacks(customEvents, props));
+  const nonEventProps = omitEventCallbacks(customEvents, props);
+  const nativeProps = {};
+
+  for (const p in nonEventProps) {
+    nativeProps[p.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()] = nonEventProps[p];
+  }
+
   if (typeof window !== 'undefined') {
     const innerRef = useRef();
     useImperativeHandle(ref, () => innerRef.current);
     useEventListeners(innerRef, customEvents, props);
-
     return React.createElement('${elementName}', { ...nativeProps, ref: innerRef }, children);
   }
 
