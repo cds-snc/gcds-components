33a9444e5ef0850a9efeacd43493bf7c9a15b067
Daine Trinidad <daine.trinidad@cds-snc.ca>
2024-01-25 10:22:58 -0800

    ci: changes to release please config

--- packages/web/stencil.config.ts
+++ packages/web/stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { postcss } from '@stencil/postcss';
 import { sass } from '@stencil/sass';
 import { inlineSvg } from 'stencil-inline-svg';
-import { reactOutputTarget as react } from '@stencil/react-output-target';
+import { reactOutputTarget } from '@stencil/react-output-target';
 import { angularOutputTarget } from '@stencil/angular-output-target';
 
 export const config: Config = {
@@ -10,12 +10,14 @@ export const config: Config = {
   globalStyle: './node_modules/@cdssnc/gcds-tokens/build/web/css/tokens.css',
   srcDir: './src',
   outputTargets: [
-    react({
+    reactOutputTarget({
       componentCorePackage: '@cdssnc/gcds-components',
       proxiesFile: '../react/src/components/stencil-generated/index.ts',
       includeDefineCustomElements: true,
     }),
     angularOutputTarget({
+      // outputType should be set to 'component' for Stencil projects using the dist output. Otherwise if using the custom elements output, outputType should be set to 'scam' or 'standalone'.
+      outputType: 'component',
       componentCorePackage: '@cdssnc/gcds-components',
       directivesProxyFile: '../angular/src/lib/stencil-generated/components.ts',
       directivesArrayFile: '../angular/src/lib/stencil-generated/index.ts',
