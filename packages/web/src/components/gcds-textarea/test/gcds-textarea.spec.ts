9b7310c47b3d4a30568f6f3dac79a0414790ab8b
Melanie Boeckmann <melanie.bockmann@gmail.com>
2024-02-01 08:54:22 -0800

    refactor: css rewrite textarea (#400)
    refactor: CSS rewrite

--- packages/web/src/components/gcds-textarea/test/gcds-textarea.spec.ts
+++ packages/web/src/components/gcds-textarea/test/gcds-textarea.spec.ts
@@ -44,7 +44,7 @@ describe('gcds-textarea', () => {
             rows="5"
             maxlength="22"
           >Value Test</textarea>
-          <p id="textarea__count-character-count" aria-live="polite">12 characters left</p>
+          <p id="textarea__count-character-count" class="textarea__count" aria-live="polite">12 characters left</p>
         </div>
       </gcds-textarea>
     `);
@@ -68,7 +68,7 @@ describe('gcds-textarea', () => {
             rows="5"
             maxlength="22"
           >Value Test</textarea>
-          <p id="textarea__count-character-count" aria-live="polite">12 caractères restants</p>
+          <p id="textarea__count-character-count" class="textarea__count" aria-live="polite">12 caractères restants</p>
         </div>
       </gcds-textarea>
     `);
