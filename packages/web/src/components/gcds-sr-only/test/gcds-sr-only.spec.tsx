c211e693249b5b179e5dac3f23ce5d842aab3fa2
Melanie Boeckmann <melanie.bockmann@gmail.com>
2024-03-20 15:07:04 -0700

    feat: add tag prop to gcds-sr-only component

--- packages/web/src/components/gcds-sr-only/test/gcds-sr-only.spec.tsx
+++ packages/web/src/components/gcds-sr-only/test/gcds-sr-only.spec.tsx
@@ -10,7 +10,43 @@ describe('gcds-sr-only', () => {
     expect(page.root).toEqualHtml(`
       <gcds-sr-only>
         <mock:shadow-root>
-          <slot></slot>
+          <p>
+            <slot></slot>
+          </p>
+        </mock:shadow-root>
+        Hidden text
+      </gcds-sr-only>
+    `);
+  });
+
+  it('renders p tag if invalid tag value', async () => {
+    const page = await newSpecPage({
+      components: [GcdsSrOnly],
+      html: `<gcds-sr-only tag="aside">Hidden text</gcds-sr-only>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <gcds-sr-only tag="aside">
+        <mock:shadow-root>
+          <p>
+            <slot></slot>
+          </p>
+        </mock:shadow-root>
+        Hidden text
+      </gcds-sr-only>
+    `);
+  });
+
+  it('renders heading tag', async () => {
+    const page = await newSpecPage({
+      components: [GcdsSrOnly],
+      html: `<gcds-sr-only tag="h2">Hidden text</gcds-sr-only>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <gcds-sr-only tag="h2">
+        <mock:shadow-root>
+          <h2>
+            <slot></slot>
+          </h2>
         </mock:shadow-root>
         Hidden text
       </gcds-sr-only>
