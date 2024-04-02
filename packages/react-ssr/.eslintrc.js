698f9828ebe3a99cc0d3ae3cc303cf5745ce8922
Daine Trinidad <daine.trinidad@cds-snc.ca>
2024-04-02 12:38:22 -0700

    refactor: rename directory to react-ssr (#490)
    * refactor: rename directory to react-ssr
    
    * refactor: change workspace path for ssr as well

--- packages/react-ssr/.eslintrc.js
+++ packages/react-ssr/.eslintrc.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   extends: ['prettier', '@smartive/eslint-config'],
   parserOptions: {
-    project: ['./packages/ssr/tsconfig.eslint.json'],
+    project: ['./packages/react-ssr/tsconfig.eslint.json'],
     tsconfigRootDir: path.resolve(__dirname, '../../'),
   },
 };
