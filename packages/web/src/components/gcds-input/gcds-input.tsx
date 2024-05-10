68b623742c73382369451cf90867f7cf72601024
Ethan Wallace <ethan.wallace.91@gmail.com>
2024-05-10 08:10:20 -0400

    fix: Emit change event

--- packages/web/src/components/gcds-input/gcds-input.tsx
+++ packages/web/src/components/gcds-input/gcds-input.tsx
@@ -197,8 +197,13 @@ export class GcdsInput {
     this.value = val;
     this.internals.setFormValue(val ? val : null);
 
+    if (e.type === 'change') {
+      const changeEvt = new e.constructor(e.type, e);
+      this.el.dispatchEvent(changeEvt);
+    }
+
     customEvent.emit(this.value);
-  }
+  };
 
   /**
    * Emitted when the input has changed.
