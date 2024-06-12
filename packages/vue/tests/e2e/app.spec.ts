adb16689f6f4aa083d4242d3476422c095c968ce
Ethan Wallace <ethan.wallace.91@gmail.com>
2024-06-12 13:51:34 -0400

    chore: Vue package tests (#555)
    * feat: Add/create @cdssnc/gcds-components-vue package
    
    * Add example to readme
    
    * Add French section to readme
    
    * Update @cdssnc/gcds-components version
    
    * Update packages/vue/README.md
    
    Co-authored-by: Daine Trinidad <daine.trinidad@cds-snc.ca>
    
    * Solve build error
    
    * Update readmes and release-please
    
    * fix: small typo in vue readme
    
    * first basic test using cypress
    
    * Convert to using playwright
    
    * Fix dependencies + remove extra files
    
    * Add playwright dependencies to test workflow
    
    * PR feedback - make tests and documentation more specific
    
    * PR feedback: rename files + add new directory
    
    * PR feddback: remove comment
    
    ---------
    
    Co-authored-by: Daine Trinidad <daine.trinidad@cds-snc.ca>

---
+++ packages/vue/tests/e2e/app.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+
+test('Page has loaded and rendered', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Vue app for E2E tests/);
+
+  // Topic menu has rendered
+  const topicMenuButton = await page.locator('gcds-topic-menu button');
+  await expect(topicMenuButton).toHaveText('Main Menu');
+
+  // Breadcrumbs has rendered
+  const breadcrumbsLink = await page.locator('gcds-breadcrumbs-item');
+  await expect(breadcrumbsLink).toHaveText('Canada.ca');
+
+  // Footer has rendered
+  const footerLinks = await page.locator('gcds-footer a').count();
+  await expect(footerLinks).toBe(26);
+});
+
+test('Select component event handling', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  // Change normal HTML select option
+  await page.locator('#color').selectOption('yellow');
+  await expect(page.locator('#selectedColor')).toHaveText('yellow');
+
+  // Change gcds-select with events
+  await page.locator('#gcds-event select').selectOption('green');
+  await expect(page.locator('#selectedColor')).toHaveText('green');
+});
+
+test('Select component input binding with v-model', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  // Change normal HTML select option
+  await page.locator('#colorhtml').selectOption('blue');
+  await expect(page.locator('#selectedColor')).toHaveText('blue');
+
+  // Change gcds-select with v-model
+  await page.locator('#gcds-v-model select').selectOption('yellow');
+  await expect(page.locator('#selectedColor')).toHaveText('yellow');
+});
