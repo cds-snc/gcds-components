5ec470faa9832e75cdffa0d2fd385fc1c7c0beb7
Daine Trinidad <daine.trinidad@cds-snc.ca>
2024-06-18 15:13:58 -0700

    refactor: separate the cdn upload process and add a retry to wait for npm publishing to complete

---
+++ utils/scripts/upload_to_cdn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the published package name
+PUBLISHED_PACKAGE="${{ steps.publish.outputs.id }}"
+
+mkdir -p ./tmp \
+  && sleep 60 \
+  && npm install --prefix ./tmp "$PUBLISHED_PACKAGE" \
+  && cd ./tmp/node_modules
+
+aws s3 sync ./${{ matrix.name }} s3://${{ env.CDN_BUCKET }}/"$PUBLISHED_PACKAGE" --delete
+aws s3 sync ./${{ matrix.name }} s3://${{ env.CDN_BUCKET }}/${{ matrix.name }}@latest --delete
+aws s3api head-object --bucket ${{ env.CDN_BUCKET }} --key "$PUBLISHED_PACKAGE"/package.json
+aws s3api head-object --bucket ${{ env.CDN_BUCKET }} --key ${{ matrix.name }}@latest/package.json
+
+aws cloudfront create-invalidation --distribution-id ${{ secrets.CDN_CLOUDFRONT_DIST_ID }} --paths "/*"
