3cbe8bc08d021849fa16269800fb08e12d4db037
Pierre Dubois <duboisp2@gmail.com>
2024-09-17 12:43:04 -0400

    fix(gcds-sr-only): Align with WCAG technique C7 (#642)

--- packages/web/src/components/gcds-sr-only/test/gcds-sr-only.e2e.ts
+++ packages/web/src/components/gcds-sr-only/test/gcds-sr-only.e2e.ts
@@ -8,4 +8,29 @@ describe('gcds-sr-only', () => {
     const element = await page.find('gcds-sr-only');
     expect(element).toHaveClass('hydrated');
   });
+  
+  it('Check that the element confines its display to a pixel and hides the text', async () => {
+    const page = await newE2EPage();
+    await page.setContent( `
+      <div style="padding: 20px; background-color:#8d201c; color:white">Hello</div>
+      <gcds-sr-only></gcds-sr-only>
+      <div style="padding: 20px; background-color:#2a7da6; color:white;">World</div>
+    `);
+
+    const sizingInfo = await page.evaluate( () => {
+      const elm = document.querySelector( 'gcds-sr-only' );
+      const rectPrevElm = elm.previousElementSibling.getBoundingClientRect();
+      const rectNextElm = elm.nextElementSibling.getBoundingClientRect();
+      return {
+        offsetHeight: elm.offsetHeight,
+        offsetWidth: elm.offsetWidth,
+        bottomPrevElm: rectPrevElm.bottom,
+        topNextElm: rectNextElm.top
+      }
+    });
+
+    expect( sizingInfo.bottomPrevElm ).toEqual( sizingInfo.topNextElm );
+    expect( sizingInfo.offsetHeight ).toBeGreaterThan( 0 );
+    expect( sizingInfo.offsetWidth ).toBeGreaterThan( 0 );
+  });
 });
