146e8b29fbd216d655c93266fed0185b540b1712
Ethan Wallace <ethan.wallace.91@gmail.com>
2024-10-08 09:46:39 -0400

    refactor(gcds-signature): Signature alternative text to be bilingual (#660)
    * refactor(gcds-signature): Signature alternative text to be bilingual
    
    * Optimize rendering

--- packages/web/src/components/gcds-signature/i18n/i18n.js
+++ packages/web/src/components/gcds-signature/i18n/i18n.js
@@ -1,9 +1,11 @@
 const I18N = {
   en: {
     link: 'https://canada.ca/en.html',
+    gc: 'Government of Canada',
   },
   fr: {
     link: 'https://canada.ca/fr.html',
+    gc: 'Gouvernement du Canada',
   },
 };
 
