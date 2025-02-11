16863a43b10bec032b198db25c465adfd9122b3e
Cameron Radmore <55817894+radmorecameron@users.noreply.github.com>
2025-02-11 01:16:24 -0500

    chore: Migrate prettierrc.json to typed prettier.config.mjs file

---
+++ prettier.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('prettier').Config} */
+export default {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  jsxSingleQuote: false,
+  quoteProps: "consistent",
+  printWidth: 80,
+  semi: true,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+}
