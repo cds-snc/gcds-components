50b4c71db8cae66d189ca7996060f84daf35d5b7
Peter Rushforth <peter.rushforth@gmail.com>
2025-02-25 16:05:55 -0500

    More formatting

--- packages/web/src/components/gcds-map/stories/gcds-map.stories.tsx
+++ packages/web/src/components/gcds-map/stories/gcds-map.stories.tsx
@@ -16,45 +16,89 @@ export default {
 
   argTypes: {
     lat: {
-      name: 'Latitude',
+      name: 'lat',
       control: { type: 'number' },
-      defaultValue: 66.5
+      table: {
+        type: { summary: 'A decimal number between +/- 90.  Positive numbers are north of the equator. Latitudes which are meaningful depend on the projection system used.' },
+        defaultValue: { summary: '0' }
+      }
     },
     lon: {
-      name: 'Longitude',
-      control: { type: 'number' },
-      defaultValue: -106
+      name: 'lon',
+      control: { type: 'number', min: -180.0, max: +180.0 },
+      table: {
+        type: { summary: 'A decimal number between +/- 180.  Positive numbers are east of the meridian at Greenwich, UK.  Longitudes which are meaningful depend on the projection system used. ' },
+        defaultValue: { summary: '0' }
+      }
     },
     zoom: {
-      name: 'Zoom Level',
+      name: 'zoom',
       control: { type: 'number' },
-      defaultValue: 2
+      table: {
+        type: { summary: 'A non-negative integer, up to 26, used as a proxy for map scale. The map scale associated to a zoom value depends on the tiled coordinate reference system.' },
+        defaultValue: { summary: '0' }
+      }
     },
     projection: {
-      name: 'Projection',
+      name: 'projection',
       control: { type: 'select' },
       options: ['OSMTILE', 'CBMTILE','APSTILE', 'WGS84'],
-      defaultValue: 'OSMTILE'
+      table: {
+        type: { summary: 'A case-sensitive string token identifier of a standard or custom MapML tiled coordinated reference system. The "\:" character is not allowed.' },
+        defaultValue: { summary: 'OSMTILE' }
+      }
     },
     layer: {
-      name: 'Layers',
+      name: '\<map-layer\>',
       control: 'select',
       options: Object.keys(layerMap),
       mapping: layerMap,
-      defaultValue: '/dist/gcds/gcds-map/mapml/en/cbmtile/toporama'
+      table: {
+        type: { summary: 'Layers are specified by one or more \<map-layer\> elements.' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    lang: {
+      name: 'lang',
+      control: { type: 'select' },
+      options: ['en', 'fr'],
+      table: {
+        type: { summary: 'The language of the map viewer user interface. May be specified on any ancestor element, especially \<html lang=\>.' },
+        defaultValue: { summary: 'en' },
+      },
+    },
+    controls: {
+      name: 'controls',
+      control: 'boolean',
+      table: {
+        type: { summary: 'Enables or removes optional map controls' },
+        defaultValue: { summary: 'false, equivalent to absent controls attribute' },
+      },
+    },
+    static: {
+      name: 'static',
+      control: 'boolean',
+      table: {
+        type: { summary: 'Disables map events and most interactive behaviour. Typically used in conjunction with controls attribute.' },
+        defaultValue: { summary: 'false, equivalent to absent static attribute, and the result is an interactive map.' }
+      }
     },
     controlslist: {
       name: 'controlslist',
       control: 'multi-select',
       options: ['geolocation', 'nofullscreen', 'nozoom', 'nolayer', 'noreload', 'noscale'],
       table: {
-        type: { summary: 'Space-separated list of case-insensitive string tokens of control name or "no"+control name, if the control is a default control' },
-        defaultValue: { summary: 'Empty string' },
+        type: { summary: 'Space-separated list of case-insensitive string tokens of control name or "no"+control name' },
+        defaultValue: { summary: 'Empty string, equivalent to absent controlslist attribute.' },
       },
     },
     caption: {
-      name: 'Caption',
+      name: '\<map-caption\>',
       control: { type: 'text' },
+      table: {
+        type: { summary: 'A string describing the purpose of the map. The \<map-caption\> element can be used to provide a screen reader-friendly caption or title for the map.' },
+        defaultValue: { summary: '-' },
+      },
     }
   }
 };
@@ -63,7 +107,7 @@ export default {
 // "Show Code" disclosure widget; don't change it without testing the result...
 const TemplateBasic = (args) => {
   return `
-    <mapml-viewer lat="${args.lat}" lon="${args.lon}" zoom="${args.zoom}" projection="${args.projection}"${args.controls ? ' controls' : ''}${args.static ? ' static' : ''}${args.controlslist.length > 0  ? ` controlslist="${args.controlslist.join(' ')}"` : ''}>
+    <mapml-viewer lat="${args.lat}" lon="${args.lon}" zoom="${args.zoom}" lang="${args.lang}" projection="${args.projection}"${args.controls ? ' controls' : ''}${args.static ? ' static' : ''}${args.controlslist.length > 0  ? ` controlslist="${args.controlslist.join(' ')}"` : ''}>
 
   <map-caption>${args.caption}</map-caption>
 
@@ -81,6 +125,7 @@ Default.args = {
   projection: 'OSMTILE',
   controls: true,
   static: false,
+  lang: 'en',
   controlslist: ['geolocation'],
   layer: '/dist/gcds/gcds-map/mapml/en/osmtile/cbmt',
   caption: 'A map of Canada'
