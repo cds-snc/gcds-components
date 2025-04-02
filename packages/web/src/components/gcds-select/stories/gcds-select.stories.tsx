b2e392f151e5a1930999348db8316e43f530d07a
Ethan Wallace <ethan.wallace.91@gmail.com>
2025-04-02 14:31:39 -0400

    storybook: Update/fix components events & properties table (#797)
    * fix: date-input compact type label
    
    * fix: input autocomplete to open text field
    
    * fix: radio-group listed events
    
    * fix: Add slots to stories for fieldset and select

--- packages/web/src/components/gcds-select/stories/gcds-select.stories.tsx
+++ packages/web/src/components/gcds-select/stories/gcds-select.stories.tsx
@@ -97,6 +97,18 @@ export default {
     ...validatorProps,
     ...langProp,
 
+    // Slots
+    default: {
+      control: {
+        type: 'text',
+      },
+      description:
+        'Customize the content or include additional elements. Accepts `<options>` and `<optgroup>` elements. | Personnalisez le contenu ou ajoutez des éléments supplémentaires. Accepte les éléments `<options>` et `<optgroup>`.',
+      table: {
+        category: 'Slots | Fentes',
+      },
+    },
+
     // Events
     gcdsChange: {
       action: 'change',
@@ -112,6 +124,18 @@ export default {
     },
   },
 };
+
+const selectOptions = `<option value="1">Option 1</option>
+  <option value="2">Option 2</option>
+  <option value="3">Option 3</option>
+  <option value="4">Option 4</option>
+  <optgroup label="Group">
+    <option value="5">Option 5</option>
+    <option value="6">Option 6</option>
+    <option value="7">Option 7</option>
+    <option value="8">Option 8</option>
+  </optgroup>
+`;
 
 const Template = args =>
   `
@@ -129,14 +153,7 @@ const Template = args =>
   ${args.validateOn != 'blur' ? `validate-on="${args.validateOn}"` : null}
   ${args.lang != 'en' ? `lang="${args.lang}"` : null}
 >
-  <option value="1">Option 1</option>
-  <option value="2">Option 2</option>
-  <option value="3">Option 3</option>
-  <option value="4">Option 4</option>
-  <option value="5">Option 5</option>
-  <option value="6">Option 6</option>
-  <option value="7">Option 7</option>
-  <option value="8">Option 8</option>
+  ${args.default}
 </gcds-select>
 
 <!-- React code -->
@@ -153,14 +170,7 @@ const Template = args =>
   ${args.validateOn != 'blur' ? `validateOn="${args.validateOn}"` : null}
   ${args.lang != 'en' ? `lang="${args.lang}"` : null}
 >
-  <option value="1">Option 1</option>
-  <option value="2">Option 2</option>
-  <option value="3">Option 3</option>
-  <option value="4">Option 4</option>
-  <option value="5">Option 5</option>
-  <option value="6">Option 6</option>
-  <option value="7">Option 7</option>
-  <option value="8">Option 8</option>
+  ${args.default}
 </GcdsSelect>
 `.replace(/\s\snull\n/g, '');
 
@@ -178,14 +188,7 @@ const TemplatePlayground = args => `
   ${args.validateOn != 'blur' ? `validate-on="${args.validateOn}"` : null}
   ${args.lang != 'en' ? `lang="${args.lang}"` : null}
 >
-  <option value="1">Option 1</option>
-  <option value="2">Option 2</option>
-  <option value="3">Option 3</option>
-  <option value="4">Option 4</option>
-  <option value="5">Option 5</option>
-  <option value="6">Option 6</option>
-  <option value="7">Option 7</option>
-  <option value="8">Option 8</option>
+  ${args.default}
 </gcds-select>
 `;
 
@@ -204,6 +207,7 @@ Default.args = {
   disabled: false,
   lang: 'en',
   validateOn: 'blur',
+  default: selectOptions,
 };
 
 // ------ Select states ------
@@ -218,6 +222,7 @@ Disabled.args = {
   disabled: true,
   lang: 'en',
   validateOn: 'blur',
+  default: selectOptions,
 };
 
 export const Error = Template.bind({});
@@ -231,6 +236,7 @@ Error.args = {
   errorMessage: 'Error message or validation message.',
   lang: 'en',
   validateOn: 'blur',
+  default: selectOptions,
 };
 
 export const Required = Template.bind({});
@@ -243,6 +249,7 @@ Required.args = {
   required: true,
   lang: 'en',
   validateOn: 'blur',
+  default: selectOptions,
 };
 
 // ------ Select without default value ------
@@ -255,6 +262,7 @@ WithoutDefaultValue.args = {
   hint: 'Hint / Example message.',
   lang: 'en',
   validateOn: 'blur',
+  default: selectOptions,
 };
 
 // ------ Select events & properties ------
@@ -272,6 +280,7 @@ Props.args = {
   disabled: false,
   lang: 'en',
   validateOn: 'blur',
+  default: selectOptions,
 };
 
 // ------ Select playground ------
@@ -289,4 +298,5 @@ Playground.args = {
   disabled: false,
   lang: 'en',
   validateOn: 'blur',
+  default: selectOptions,
 };
