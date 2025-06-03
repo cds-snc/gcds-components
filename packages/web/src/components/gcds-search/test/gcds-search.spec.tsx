d2e4ef890814de2381008079d4e8618db4593308
Melanie Boeckmann <melanie.bockmann@gmail.com>
2025-06-03 11:37:25 -0700

    fix: align structural markup for link and search with GCWeb (#859)

--- packages/web/src/components/gcds-search/test/gcds-search.spec.tsx
+++ packages/web/src/components/gcds-search/test/gcds-search.spec.tsx
@@ -10,18 +10,18 @@ describe('gcds-search', () => {
     expect(page.root).toEqualHtml(`
       <gcds-search>
         <mock:shadow-root>
-          <div class="gcds-search">
+          <section class="gcds-search">
             <gcds-sr-only tag="h2">
               Search
             </gcds-sr-only>
             <form action="https://www.canada.ca/en/sr/srb.html" class="gcds-search__form" method="get" role="search">
               <gcds-label hide-label="" label="Search Canada.ca" label-for="search"></gcds-label>
-              <input class="gcds-search__input" id="search" list="search-list" maxlength="170" name="q" placeholder="Search Canada.ca" size="34" type="search">
+              <input class="gcds-search__input" id="search" maxlength="170" name="q" placeholder="Search Canada.ca" size="34" type="search">
               <gcds-button class="gcds-search__button" exportparts="button" type="submit">
                 <gcds-icon label="Search" name="search" size="h3"></gcds-icon>
               </gcds-button>
             </form>
-          </div>
+          </section>
         </mock:shadowroot>
       </gcds-search>
     `);
@@ -34,18 +34,18 @@ describe('gcds-search', () => {
     expect(page.root).toEqualHtml(`
       <gcds-search lang="fr">
         <mock:shadow-root>
-          <div class="gcds-search">
+          <section class="gcds-search">
             <gcds-sr-only tag="h2">
               Recherche
             </gcds-sr-only>
             <form action="https://www.canada.ca/fr/sr/srb.html" class="gcds-search__form" method="get" role="search">
               <gcds-label hide-label="" label="Rechercher dans Canada.ca" label-for="search"></gcds-label>
-              <input class="gcds-search__input" id="search" list="search-list" maxlength="170" name="q" placeholder="Rechercher dans Canada.ca" size="34" type="search">
+              <input class="gcds-search__input" id="search" maxlength="170" name="q" placeholder="Rechercher dans Canada.ca" size="34" type="search">
               <gcds-button class="gcds-search__button" exportparts="button" type="submit">
                 <gcds-icon label="Recherche" name="search" size="h3"></gcds-icon>
               </gcds-button>
             </form>
-          </div>
+          </section>
         </mock:shadowroot>
       </gcds-search>
     `);
@@ -64,18 +64,18 @@ describe('gcds-search', () => {
     expect(page.root).toEqualHtml(`
       <gcds-search action="submit.html" method="post" name="s" placeholder="Text.ca" search-id="searchForm">
         <mock:shadow-root>
-          <div class="gcds-search">
+          <section class="gcds-search">
             <gcds-sr-only tag="h2">
               Search
             </gcds-sr-only>
             <form action="submit.html" class="gcds-search__form" method="post" role="search">
             <gcds-label hide-label="" label="Search Text.ca" label-for="searchForm"></gcds-label>
-              <input class="gcds-search__input" id="searchForm" list="search-list" maxlength="170" name="s" placeholder="Search Text.ca" size="34" type="search">
+              <input class="gcds-search__input" id="searchForm" maxlength="170" name="s" placeholder="Search Text.ca" size="34" type="search">
               <gcds-button class="gcds-search__button" exportparts="button" type="submit">
                 <gcds-icon label="Search" name="search" size="h3"></gcds-icon>
               </gcds-button>
             </form>
-          </div>
+          </section>
         </mock:shadowroot>
       </gcds-search>
     `);
