0980670755530d3d7567d7f8d50264841796f378
Ethan Wallace <ethan.wallace.91@gmail.com>
2025-06-23 11:47:05 -0400

    PR feedback: router link directive cleanup

--- packages/angular/src/lib/directives/gcds-router-link.ts
+++ packages/angular/src/lib/directives/gcds-router-link.ts
@@ -10,7 +10,6 @@ export class GcdsRouterDirective implements OnInit {
   @Input() routerLink!: string | any[];
   @Input() queryParams?: { [k: string]: any };
   @Input() fragment?: string;
-  @Input() routerLinkActive?: string;
 
   constructor(private router: Router) {}
 
@@ -20,12 +19,15 @@ export class GcdsRouterDirective implements OnInit {
   onGcdsClick(event: CustomEvent) {
     event.preventDefault();
 
-    const commands =
-      this.routerLink !== '' && this.routerLink != null
-        ? this.routerLink
-        : typeof event.detail === 'object' && event.detail !== null
-          ? event.detail.href
-          : event.detail;
+    let commands;
+
+    if (this.routerLink !== '' && this.routerLink != null) {
+      commands = this.routerLink;
+    } else if (typeof event.detail === 'object' && event.detail !== null) {
+      commands = event.detail.href;
+    } else {
+      commands = event.detail;
+    }
 
     const extras: NavigationExtras = {
       queryParams: this.queryParams,
@@ -35,7 +37,6 @@ export class GcdsRouterDirective implements OnInit {
     if (Array.isArray(commands)) {
       this.router.navigate(commands, extras);
     } else if (typeof commands === 'string') {
-      console.log('I am here', commands);
       this.router.navigateByUrl(commands, extras);
     } else {
       console.warn('Invalid routerLink or event detail:', commands);
