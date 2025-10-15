cee4e53f9f8f9f3afd2b2129bc64945ec67a80c9
Ethan Wallace <ethan.wallace.91@gmail.com>
2025-10-15 08:51:00 -0400

    Test for initial values

--- packages/angular/tests/e2e/ngmodel.spec.ts
+++ packages/angular/tests/e2e/ngmodel.spec.ts
@@ -4,14 +4,17 @@ test('Form component ngModel', async ({ page }) => {
   await page.goto('http://localhost:4200/forms');
 
   // Change name gcds-input
+  await expect(page.locator('#input-name')).toHaveText('');
   await page.locator('input[name="name"]').fill('John');
   await expect(page.locator('#input-name')).toHaveText('John');
 
   // Change number gcds-input
+  await expect(page.locator('#input-number')).toHaveText('0');
   await page.locator('input[name="number"]').fill('23');
   await expect(page.locator('#input-number')).toHaveText('23');
 
   // Change textarea
+  await expect(page.locator('#input-message')).toHaveText('');
   await page
     .locator('textarea[name="message"]')
     .fill('This is a message showing ngModel working correctly.');
@@ -20,6 +23,7 @@ test('Form component ngModel', async ({ page }) => {
   );
 
   // Change full gcds-date-input
+  await expect(page.locator('#input-date')).toHaveText('');
   await page
     .locator('gcds-date-input[name="date-full"] input[name="day"]')
     .fill('25');
@@ -43,10 +47,12 @@ test('Form component ngModel', async ({ page }) => {
   await expect(page.locator('#input-date')).toHaveText('2024-12');
 
   // Change gcds-select
+  await expect(page.locator('#input-select')).toHaveText('');
   await page.locator('gcds-select select[name="select"]').selectOption('3');
   await expect(page.locator('#input-select')).toHaveText('3');
 
   // Change gcds-radios
+  await expect(page.locator('#input-radios')).toHaveText('');
   await page
     .locator('gcds-radios input[value="radio2"]')
     .check({ force: true });
@@ -57,6 +63,7 @@ test('Form component ngModel', async ({ page }) => {
   await expect(page.locator('#input-radios')).toHaveText('radio1');
 
   // Change gcds-checkboxes
+  await expect(page.locator('#input-check')).toHaveText('');
   await page
     .locator('gcds-checkboxes input[value="check2"]')
     .check({ force: true });
@@ -65,4 +72,8 @@ test('Form component ngModel', async ({ page }) => {
     .locator('gcds-checkboxes input[value="check1"]')
     .check({ force: true });
   await expect(page.locator('#input-check')).toHaveText('check2,check1');
+  await page
+    .locator('gcds-checkboxes input[value="check2"]')
+    .uncheck({ force: true });
+  await expect(page.locator('#input-check')).toHaveText('check1');
 });
