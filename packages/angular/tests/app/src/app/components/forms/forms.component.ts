5381fb8e606af1858c096d0e9fa94c5e304f5119
Ethan Wallace <ethan.wallace.91@gmail.com>
2025-10-15 09:27:25 -0400

    Add tests to set and reset values

--- packages/angular/tests/app/src/app/components/forms/forms.component.ts
+++ packages/angular/tests/app/src/app/components/forms/forms.component.ts
@@ -16,140 +16,169 @@ import { FormsModule } from '@angular/forms';
 
     <gcds-heading tag="h1">Form components</gcds-heading>
 
-    <section class="b-solid p-300 mb-300" id="input">
-      <gcds-heading tag="h2" margin-top="0">Gcds-input</gcds-heading>
-      <p>Text input and number input</p>
+    <form>
+      <section class="b-solid p-300 mb-300" id="input">
+        <gcds-heading tag="h2" margin-top="0">Gcds-input</gcds-heading>
+        <p>Text input and number input</p>
 
-      <gcds-input
-        type="text"
-        input-id="name"
-        name="name"
-        label="Full name"
-        [(ngModel)]="name"
-      ></gcds-input>
+        <gcds-input
+          type="text"
+          input-id="name"
+          name="name"
+          label="Full name"
+          [(ngModel)]="name"
+        ></gcds-input>
 
-      <gcds-input
-        type="number"
-        input-id="number"
-        name="number"
-        label="Number"
-        [(ngModel)]="number"
-      ></gcds-input>
+        <gcds-input
+          type="number"
+          input-id="number"
+          name="number"
+          label="Number"
+          [(ngModel)]="number"
+        ></gcds-input>
 
-      <p>
-        You entered: <span id="input-name">{{ name }}</span>
-      </p>
-      <p>
-        You entered: <span id="input-number">{{ number }}</span>
-      </p>
-    </section>
-
-    <section class="b-solid p-300 mb-300" id="textarea">
-      <gcds-heading tag="h2" margin-top="0">Gcds-textarea</gcds-heading>
-
-      <gcds-textarea
-        label="Message"
-        textarea-id="message"
-        name="message"
-        [(ngModel)]="message"
-      ></gcds-textarea>
-
-      <p>
-        You entered: <span id="input-message">{{ message }}</span>
-      </p>
-    </section>
-
-    <section class="b-solid p-300 mb-300" id="date-input">
-      <gcds-heading tag="h2" margin-top="0">Gcds-date-input</gcds-heading>
-      <p>Date input in full and compact format.</p>
-
-      <gcds-date-input
-        legend="Full date"
-        name="date-full"
-        format="full"
-        [(ngModel)]="date"
-      ></gcds-date-input>
-
-      <gcds-date-input
-        legend="Compact date"
-        name="date-compact"
-        format="compact"
-        [(ngModel)]="date"
-      ></gcds-date-input>
-
-      <div>
         <p>
-          You entered: <span id="input-date">{{ date }}</span>
+          You entered: <span id="input-name">{{ name }}</span>
         </p>
-      </div>
-    </section>
+        <p>
+          You entered: <span id="input-number">{{ number }}</span>
+        </p>
+      </section>
 
-    <section class="b-solid p-300 mb-300" id="select">
-      <gcds-heading tag="h2" margin-top="0">Gcds-select</gcds-heading>
+      <section class="b-solid p-300 mb-300" id="textarea">
+        <gcds-heading tag="h2" margin-top="0">Gcds-textarea</gcds-heading>
 
-      <gcds-select
-        label="Select"
-        select-id="select"
-        name="select"
-        default-value="Select an option"
-        [(ngModel)]="select"
-      >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-      </gcds-select>
+        <gcds-textarea
+          label="Message"
+          textarea-id="message"
+          name="message"
+          [(ngModel)]="message"
+        ></gcds-textarea>
 
-      <p>
-        You entered: <span id="input-select">{{ select }}</span>
-      </p>
-    </section>
+        <p>
+          You entered: <span id="input-message">{{ message }}</span>
+        </p>
+      </section>
 
-    <section class="b-solid p-300 mb-300" id="radios">
-      <gcds-heading tag="h2" margin-top="0">Gcds-radios</gcds-heading>
+      <section class="b-solid p-300 mb-300" id="date-input">
+        <gcds-heading tag="h2" margin-top="0">Gcds-date-input</gcds-heading>
+        <p>Date input in full and compact format.</p>
 
-      <gcds-radios
-        name="radio"
-        legend="Radios"
-        options='[
+        <gcds-date-input
+          legend="Full date"
+          name="date-full"
+          format="full"
+          [(ngModel)]="dateFull"
+        ></gcds-date-input>
+
+        <gcds-date-input
+          legend="Compact date"
+          name="date-compact"
+          format="compact"
+          [(ngModel)]="dateCompact"
+        ></gcds-date-input>
+
+        <div>
+          <p>
+            You entered: <span id="input-date-full">{{ dateFull }}</span>
+          </p>
+          <p>
+            You entered: <span id="input-date-compact">{{ dateCompact }}</span>
+          </p>
+        </div>
+      </section>
+
+      <section class="b-solid p-300 mb-300" id="select">
+        <gcds-heading tag="h2" margin-top="0">Gcds-select</gcds-heading>
+
+        <gcds-select
+          label="Select"
+          select-id="select"
+          name="select"
+          default-value="Select an option"
+          [(ngModel)]="select"
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </gcds-select>
+
+        <p>
+          You entered: <span id="input-select">{{ select }}</span>
+        </p>
+      </section>
+
+      <section class="b-solid p-300 mb-300" id="radios">
+        <gcds-heading tag="h2" margin-top="0">Gcds-radios</gcds-heading>
+
+        <gcds-radios
+          name="radio"
+          legend="Radios"
+          options='[
           { "label": "Label for radio 1", "id": "radio1", "value": "radio1"},
           { "label": "Label for radio 2", "id": "radio2", "value": "radio2"},
           { "label": "Label for radio 3", "id": "radio3", "value": "radio3"}
         ]'
-        [(ngModel)]="radio"
-      ></gcds-radios>
+          [(ngModel)]="radio"
+        ></gcds-radios>
 
-      <p>
-        You entered: <span id="input-radios">{{ radio }}</span>
-      </p>
-    </section>
+        <p>
+          You entered: <span id="input-radios">{{ radio }}</span>
+        </p>
+      </section>
 
-    <section class="b-solid p-300 mb-300" id="checkboxes">
-      <gcds-heading tag="h2" margin-top="0">Gcds-checkboxes</gcds-heading>
+      <section class="b-solid p-300 mb-300" id="checkboxes">
+        <gcds-heading tag="h2" margin-top="0">Gcds-checkboxes</gcds-heading>
 
-      <gcds-checkboxes
-        name="check"
-        legend="Checkboxes"
-        options='[
+        <gcds-checkboxes
+          name="check"
+          legend="Checkboxes"
+          options='[
             { "label": "Label for checkbox 1", "id": "check1", "value": "check1"},
             { "label": "Label for checkbox 2", "id": "check2", "value": "check2"},
             { "label": "Label for checkbox 3", "id": "check3", "value": "check3"}
           ]'
-        [(ngModel)]="check"
-      ></gcds-checkboxes>
+          [(ngModel)]="check"
+        ></gcds-checkboxes>
 
-      <p>
-        You entered: <span id="input-check">{{ check }}</span>
-      </p>
-    </section>
+        <p>
+          You entered: <span id="input-check">{{ check }}</span>
+        </p>
+      </section>
+
+      <gcds-button
+        type="button"
+        button-role="primary"
+        id="assignAll"
+        (click)="assignAll()"
+      >
+        Assign all form fields
+      </gcds-button>
+      <gcds-button type="reset" button-role="danger" id="reset">
+        Reset
+      </gcds-button>
+    </form>
   </div> `,
 })
 export class FormsComponent {
   name: string = '';
   message: string = '';
   select: string = '';
-  date: string = '';
+  dateFull: string = '';
+  dateCompact: string = '';
   radio: string = '';
   check: string | string[] = '';
   number: number = 0;
+
+  assignAll() {
+    this.name = 'John Doe';
+    this.message = 'This is a message showing ngModel working correctly.';
+    this.select = '3';
+    this.dateFull = '2024-12-25';
+    this.dateCompact = '2024-12';
+    this.radio = 'radio2';
+    this.check = ['check2', 'check1'];
+    this.number = 23;
+  }
 }
--- packages/angular/tests/e2e/ngmodel.spec.ts
+++ packages/angular/tests/e2e/ngmodel.spec.ts
@@ -23,7 +23,7 @@ test('Form component ngModel', async ({ page }) => {
   );
 
   // Change full gcds-date-input
-  await expect(page.locator('#input-date')).toHaveText('');
+  await expect(page.locator('#input-date-full')).toHaveText('');
   await page
     .locator('gcds-date-input[name="date-full"] input[name="day"]')
     .fill('25');
@@ -34,9 +34,10 @@ test('Form component ngModel', async ({ page }) => {
     .locator('gcds-date-input[name="date-full"] input[name="year"]')
     .fill('2024');
   await page.locator('gcds-date-input[name="date-full"]').blur();
-  await expect(page.locator('#input-date')).toHaveText('2024-12-25');
+  await expect(page.locator('#input-date-full')).toHaveText('2024-12-25');
 
   // Change compact gcds-date-input
+  await expect(page.locator('#input-date-compact')).toHaveText('');
   await page
     .locator('gcds-date-input[name="date-compact"] select[name="month"]')
     .selectOption('12');
@@ -44,7 +45,7 @@ test('Form component ngModel', async ({ page }) => {
     .locator('gcds-date-input[name="date-compact"] input[name="year"]')
     .fill('2024');
   await page.locator('gcds-date-input[name="date-compact"]').blur();
-  await expect(page.locator('#input-date')).toHaveText('2024-12');
+  await expect(page.locator('#input-date-compact')).toHaveText('2024-12');
 
   // Change gcds-select
   await expect(page.locator('#input-select')).toHaveText('');
@@ -76,4 +77,85 @@ test('Form component ngModel', async ({ page }) => {
     .locator('gcds-checkboxes input[value="check2"]')
     .uncheck({ force: true });
   await expect(page.locator('#input-check')).toHaveText('check1');
+});
+
+test('Form component ngModel - setting programatically', async ({ page }) => {
+  await page.goto('http://localhost:4200/forms');
+
+  await expect(page.locator('#input-name')).toHaveText('');
+  await expect(page.locator('#input-number')).toHaveText('0');
+  await expect(page.locator('#input-message')).toHaveText('');
+  await expect(page.locator('#input-date-full')).toHaveText('');
+  await expect(page.locator('#input-date-compact')).toHaveText('');
+  await expect(page.locator('#input-select')).toHaveText('');
+  await expect(page.locator('#input-radios')).toHaveText('');
+  await expect(page.locator('#input-check')).toHaveText('');
+
+  await page.locator('#assignAll').click();
+
+  await expect(page.locator('#input-name')).toHaveText('John Doe');
+  await expect(page.locator('input[name="name"]')).toHaveValue('John Doe');
+
+  await expect(page.locator('#input-number')).toHaveText('23');
+  await expect(page.locator('input[name="number"]')).toHaveValue('23');
+
+  await expect(page.locator('#input-message')).toHaveText(
+    'This is a message showing ngModel working correctly.',
+  );
+  await expect(page.locator('textarea[name="message"]')).toHaveValue(
+    'This is a message showing ngModel working correctly.',
+  );
+
+  await expect(page.locator('#input-date-full')).toHaveText('2024-12-25');
+  await expect(
+    page.locator('gcds-date-input[name="date-full"] input[name="day"]'),
+  ).toHaveValue('25');
+  await expect(
+    page.locator('gcds-date-input[name="date-full"] select[name="month"]'),
+  ).toHaveValue('12');
+  await expect(
+    page.locator('gcds-date-input[name="date-full"] input[name="year"]'),
+  ).toHaveValue('2024');
+
+  await expect(page.locator('#input-date-compact')).toHaveText('2024-12');
+  await expect(
+    page.locator('gcds-date-input[name="date-compact"] select[name="month"]'),
+  ).toHaveValue('12');
+  await expect(
+    page.locator('gcds-date-input[name="date-compact"] input[name="year"]'),
+  ).toHaveValue('2024');
+
+  await expect(page.locator('#input-select')).toHaveText('3');
+  await expect(page.locator('gcds-select select[name="select"]')).toHaveValue(
+    '3',
+  );
+
+  await expect(page.locator('#input-radios')).toHaveText('radio2');
+  await expect(page.locator('gcds-radios input[value="radio2"]')).toBeChecked();
+  await expect(
+    page.locator('gcds-radios input[value="radio1"]'),
+  ).not.toBeChecked();
+
+  await expect(page.locator('#input-check')).toHaveText('check2,check1');
+  await expect(
+    page.locator('gcds-checkboxes input[value="check2"]'),
+  ).toBeChecked();
+  await expect(
+    page.locator('gcds-checkboxes input[value="check1"]'),
+  ).toBeChecked();
+  await expect(
+    page.locator('gcds-checkboxes input[value="check3"]'),
+  ).not.toBeChecked();
+  await expect(page.locator('#input-check')).toHaveText('check2,check1');
+
+  await page.locator('#reset').click();
+
+  await expect(page.locator('#input-name')).toHaveText('');
+  await expect(page.locator('#input-number')).toHaveText('');
+  await expect(page.locator('#input-message')).toHaveText('');
+  await expect(page.locator('#input-date-full')).toHaveText('');
+  await expect(page.locator('#input-date-compact')).toHaveText('');
+  await expect(page.locator('#input-select')).toHaveText('');
+  await expect(page.locator('#input-radios')).toHaveText('');
+  await expect(page.locator('#input-check')).toHaveText('');
 });
