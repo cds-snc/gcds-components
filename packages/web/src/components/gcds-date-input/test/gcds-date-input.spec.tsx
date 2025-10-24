582d4deaaa67b7d203c768e9b42c74d97a7e263d
Cameron Radmore <cameron.radmore@sac-isc.gc.ca>
2025-10-24 08:46:47 -0400

    feat(gcds-date-input): add yyyy-mm-dd input format

--- packages/web/src/components/gcds-date-input/test/gcds-date-input.spec.tsx
+++ packages/web/src/components/gcds-date-input/test/gcds-date-input.spec.tsx
@@ -117,6 +117,121 @@ describe('gcds-date-input', () => {
     `);
   });
 
+  it('renders - yyyy-MM-dd', async () => {
+    const page = await newSpecPage({
+      components: [GcdsDateInput],
+      html: `<gcds-date-input legend="Date input" name="date" format="yyyy-MM-dd"></gcds-date-input>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <gcds-date-input format="full" legend="Date input" name="date">
+        <mock:shadow-root>
+          <fieldset aria-labelledby="date-input-legend" class="gcds-date-input__fieldset" tabindex="-1">
+            <legend id="date-input-legend">
+              Date input
+            </legend>
+            <gcds-input aria-invalid="false" class="gcds-date-input__year" inputid="year" label="Year" name="year" size="4" type="number" value=""></gcds-input>
+            <gcds-select aria-invalid="false" class="gcds-date-input__month" defaultvalue="Select a month" label="Month" name="month" selectid="month" value="">
+              <option value="01">
+                January
+              </option>
+              <option value="02">
+                February
+              </option>
+              <option value="03">
+                March
+              </option>
+              <option value="04">
+                April
+              </option>
+              <option value="05">
+                May
+              </option>
+              <option value="06">
+                June
+              </option>
+              <option value="07">
+                July
+              </option>
+              <option value="08">
+                August
+              </option>
+              <option value="09">
+                September
+              </option>
+              <option value="10">
+                October
+              </option>
+              <option value="11">
+                November
+              </option>
+              <option value="12">
+                December
+              </option>
+            </gcds-select>
+            <gcds-input aria-invalid="false" class="gcds-date-input__day" inputid="day" label="Day" name="day" size="2" type="number" value=""></gcds-input>
+          </fieldset>
+        </gcds-date-input>
+    `);
+  });
+
+  it('renders - yyyy-MM-dd - french', async () => {
+    const page = await newSpecPage({
+      components: [GcdsDateInput],
+      html: `<gcds-date-input legend="Date input" name="date" format="yyyy-MM-dd" lang="fr"></gcds-date-input>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <gcds-date-input format="full" lang="fr" legend="Date input" name="date">
+        <mock:shadow-root>
+          <fieldset aria-labelledby="date-input-legend" class="gcds-date-input__fieldset" tabindex="-1">
+            <legend id="date-input-legend">
+              Date input
+            </legend>
+            <gcds-input aria-invalid="false" class="gcds-date-input__year" inputid="year" label="Année" name="year" size="4" type="number" value=""></gcds-input>
+            <gcds-select aria-invalid="false" class="gcds-date-input__month" defaultvalue="Sélectionnez un mois" label="Mois" name="month" selectid="month" value="">
+              <option value="01">
+                janvier
+              </option>
+              <option value="02">
+                février
+              </option>
+              <option value="03">
+                mars
+              </option>
+              <option value="04">
+                avril
+              </option>
+              <option value="05">
+                mai
+              </option>
+              <option value="06">
+                juin
+              </option>
+              <option value="07">
+                juillet
+              </option>
+              <option value="08">
+                août
+              </option>
+              <option value="09">
+                septembre
+              </option>
+              <option value="10">
+                octobre
+              </option>
+              <option value="11">
+                novembre
+              </option>
+              <option value="12">
+                décembre
+              </option>
+            </gcds-select>
+            <gcds-input aria-invalid="false" class="gcds-date-input__day" inputid="day" label="Jour" name="day" size="2" type="number" value=""></gcds-input>
+          </fieldset>
+        </mock:shadow-root>
+      </gcds-date-input>
+    `);
+  });
+
   it('renders - compact', async () => {
     const page = await newSpecPage({
       components: [GcdsDateInput],
