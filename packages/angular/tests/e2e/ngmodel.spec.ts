f93c802fc22f80911840189ff4bdd2251a4f2ec0
Ethan Wallace <ethan.wallace.91@gmail.com>
2025-11-18 14:49:33 -0500

    tests(angular): Add file-uploader and error-summary tests e2e tests (#1007)
    * test: Add file-uploader and error-summary tests to angular
    
    * Update packages/angular/tests/e2e/file-uploader.spec.ts
    
    Co-authored-by: Daine Trinidad <daine.trinidad@cds-snc.ca>
    
    * Update packages/angular/tests/app/src/app/components/fileuploader/file-uploader.component.ts
    
    Co-authored-by: Daine Trinidad <daine.trinidad@cds-snc.ca>
    
    ---------
    
    Co-authored-by: Daine Trinidad <daine.trinidad@cds-snc.ca>

--- packages/angular/tests/e2e/ngmodel.spec.ts
+++ packages/angular/tests/e2e/ngmodel.spec.ts
@@ -159,3 +159,73 @@ test('Form component ngModel - setting programatically', async ({ page }) => {
   await expect(page.locator('#input-radios')).toHaveText('');
   await expect(page.locator('#input-check')).toHaveText('');
 });
+
+test(`Error summary`, async ({ page }) => {
+  await page.goto('http://localhost:4200/forms');
+
+  expect(await page.locator('gcds-error-summary a').count()).toBe(0);
+
+  await expect(page.locator('#input-name')).toHaveText('');
+  await expect(page.locator('#input-message')).toHaveText('');
+  await expect(page.locator('#input-date-full')).toHaveText('');
+  await expect(page.locator('#input-date-compact')).toHaveText('');
+  await expect(page.locator('#input-select')).toHaveText('');
+  await expect(page.locator('#input-radios')).toHaveText('');
+  await expect(page.locator('#input-check')).toHaveText('');
+
+  await page.locator('button[type="submit"]').click();
+
+  await page.waitForTimeout(500); // Wait for error summary to render
+
+  let errorLinks = await page.locator('gcds-error-summary a').all();
+
+  expect(errorLinks.length).toBe(7);
+
+  await errorLinks[0].click();
+  await expect(page.locator('input[name="name"]')).toBeFocused();
+  await page.locator('input[name="name"]').fill('John');
+
+  await errorLinks[1].click();
+  await expect(page.locator('textarea[name="message"]')).toBeFocused();
+  await page.locator('textarea[name="message"]').fill('This is a message.');
+
+  await errorLinks[2].click();
+  const dateFull = page.locator('gcds-date-input[name="date-full"]');
+
+  await expect(
+    await dateFull.evaluate(el => el === document.activeElement),
+  ).toBe(true);
+  await dateFull.evaluate(el => el.setAttribute('value', '2024-12-31'));
+
+  await errorLinks[3].click();
+  const dateCompact = page.locator('gcds-date-input[name="date-compact"]');
+
+  await expect(
+    await dateCompact.evaluate(el => el === document.activeElement),
+  ).toBe(true);
+  await dateCompact.evaluate(el => el.setAttribute('value', '2024-12'));
+
+  await errorLinks[4].click();
+  await expect(page.locator('gcds-select select[name="select"]')).toBeFocused();
+  await page.locator('gcds-select select[name="select"]').selectOption('1');
+
+  await errorLinks[5].click();
+  await expect(page.locator('gcds-radios fieldset')).toBeFocused();
+  await page
+    .locator('gcds-radios input[value="radio1"]')
+    .check({ force: true });
+
+  await errorLinks[6].click();
+  await expect(page.locator('gcds-checkboxes fieldset')).toBeFocused();
+  await page
+    .locator('gcds-checkboxes input[value="check1"]')
+    .check({ force: true });
+
+  await page.locator('button[type="submit"]').click();
+
+  await page.waitForTimeout(500); // Wait for error summary to render
+
+  errorLinks = await page.locator('gcds-error-summary a').all();
+
+  expect(errorLinks.length).toBe(0);
+});
